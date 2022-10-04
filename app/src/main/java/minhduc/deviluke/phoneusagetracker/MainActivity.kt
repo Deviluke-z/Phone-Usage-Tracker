37bfc49d786a817661c642252056c5f34d82cc42
MinhDuc1911 <dao.minh.duc.19.11@gmail.com>
2022-10-04 23:12:26 +0700

    init basic screens

---
+++ app/src/main/java/minhduc/deviluke/phoneusagetracker/MainActivity.kt
@@ -0,0 +1,11 @@
+package minhduc.deviluke.phoneusagetracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
